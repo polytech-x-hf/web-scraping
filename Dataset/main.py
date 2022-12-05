10315554053b38ba88b6d1c6e9422a417bcfee4b
Yazid BOUHRIA <rivokstv@gmail.com>
2022-12-05 11:42:41 +0100

    feat: online dataset repo init

--- Dataset/main.py
+++ Dataset/main.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 
 jojo_dataset = load_dataset(
-    "json", data_files="Dataset/jojo.json", field="items")
-
+    "json", data_files="https://huggingface.co/datasets/polytechXhf/jojos-dataset/raw/main/jojo.json", field="items")
 print("Jojo:", jojo_dataset["train"][2])
