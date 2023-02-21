372ea4d875d28174b60f714cd155e5ec8944fb9f
Alexia57 <84861815+Alexia57@users.noreply.github.com>
2023-02-21 19:55:04 +0100

    setup.py created

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package for the model with Jojo and One Piece'
+
+# Setting up
+setup(
+    name="Jojo&OnePieceModel",
+    version = VERSION,
+    # author = "",
+    # author_mail = "",
+    url="https://github.com/polytech-x-hf/web-scraping",
+    description = DESCRIPTION,
+    packages = find_packages(),
+    install_requires=[],
+    keywords=['python', 'stable diffusion'],
+    
+)
